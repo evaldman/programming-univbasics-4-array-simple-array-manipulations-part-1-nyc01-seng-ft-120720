4030fd58e03cb1c184e191affeb928baf7f7ff6e
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-13 22:10:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,13 +1,23 @@
 def using_push(array, string)
   array.push(string)
 end
-
+  
 def using_unshift(array,string)
-
+  array.unshift(string)
 end
 
 def using_pop(array)
   
 end
 
-def pop_with_args
+def pop_with_args(array)
+  
+end
+
+def using_shift(array)
+  
+end
+
+def shift_with_args(array)
+  
+end
